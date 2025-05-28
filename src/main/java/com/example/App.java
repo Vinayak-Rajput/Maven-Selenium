f93c76e7721034c54b726edcf6f9abce1abf651d
Second-Fiddle-VK <vk21112002ra@gmail.com>
2025-05-28 22:34:00 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class App 
 {
