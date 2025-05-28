965355b6114ad4b504c408d8e18ed4c601ac17d2
Second-Fiddle-VK <vk21112002ra@gmail.com>
2025-05-28 22:50:00 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,13 +2,11 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class App 
 {
     public static void main( String[] args )
     {
-         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
         driver.get("https://www.saucedemo.com");
         driver.manage().window().maximize();
